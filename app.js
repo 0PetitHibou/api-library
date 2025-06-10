28a7c692475b0eb29c4fb0424ce6c3fd5428b4cf
Mickaël <mlagarde.pro@gmail.com>
2025-06-10 08:37:27 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+
+// import bodyParser from 'body-parser';
+import express from 'express';
+import cors from 'cors';
+// session
+import session from "express-session";
+// --------------------------
+import {createUser} from "./config/db.js"
+
+
+
+const app = express();
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Lancer le serveur
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
+
+// Ajouter un utilisateur
+app.post("/users", async (req, res) => {
+  const { first_name, last_name, birth_date, mail, password } = req.body;
+  const user = await createUser(first_name, last_name, birth_date, mail, password);
+  res.status(201).send(user);
+})
+
+
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.post("/login", async (req, res) => {
+    const {mail, password} = req.body;
+
+    if (!mail || !password) {
+      return res.status(400).json({ message: "Champs manquants" });
+    }
+
+    try {
+      const db = app.get("db")();
+      const [rows] = await db.query('SELECT * FROM users WHERE mail = ?', [mail]);
+
+      if (rows.length === 0) {
+      return res.status(401).json({ error: "Utilisateur non trouvé." });
+    }
+    const user = rows[0];
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ error: "Mot de passe incorrect." });
+    }
+
+    req.session.user = {
+      id: user.id,
+      mail: user.mail,
+      first_name: user.first_name,
+      last_name: user.last_name
+    };
+    res.status(200).json({ message: "Connexion réussie." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Une erreur est survenue." });
+  }
+
+})
+
+app.get("/session", (req, res) => {
+  res.json({
+    mail: req.session.mail,
+    isLogged: !!req.session.mail
+  });
+});
---
+++ config/db.js
@@ -0,0 +1,36 @@
+import mysql from 'mysql2/promise';
+import bcrypt from 'bcrypt';
+
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '', 
+  database: 'library'
+});
+
+export async function getUser(id)
+{
+  const [rows] = await db.query('SELECT * FROM users WHERE id = ?', [id])
+  return rows[0]
+}
+
+export async function createUser(firstName ,lastName, birthDate, mail, password)
+{
+  const [rows] = await db.query('SELECT * FROM users WHERE mail = ?', [mail]);
+
+  if( rows.length === 0){
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const [result] = await db.query('INSERT INTO users(first_name, last_name, birth_date, mail, password) VALUES (?,?,?,?,?)',[firstName, lastName, birthDate, mail, hashedPassword])
+    console.log(result)
+    return {
+      id: result.insertId,
+      first_name: firstName,
+      last_name: lastName,
+      birth_date: birthDate,
+      mail: mail
+    }
+  } else {
+    return 'email déjà existant';
+  }
+} 
