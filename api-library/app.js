a10958b13fe96658dead0a5895102a2612f88f71
Mickael Lagarde <mlagarde.pro@gmail.com>
2025-12-19 15:04:07 +0100

    conflict resolution

---
+++ api-library/app.js
@@ -0,0 +1,115 @@
+// import bodyParser from 'body-parser';
+import express from 'express';
+import cors from 'cors';
+import  jwt  from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import {createUser, db, addBook, getBooks} from "../config/db.js"
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.set("db", db);
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+// Création de Middleware
+function checkToken(req, res, next) {
+	if(!req.headers.authorization) return res.status(401).send("Unauthorized"); 
+
+	// verifier le token en décriptant le token via la clé
+	jwt.verify(req.headers.authorization, process.env.JWT_SECRET, (err, decoded) => {
+		if(err) return res.status(401).send("Unauthorized");
+		req.user = {
+			id: decoded.id,
+			mail: decoded.mail
+		}
+	});
+	// appel d'un endpoint ou middleware
+	// Middleware : action qu'on fait avant endpoint
+	// endpoint :  dernier action que ta requete fait
+	next()
+}
+
+// Lancer le serveur
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`Serveur démarré sur le port ${PORT}`);
+});
+
+
+// Ajouter un utilisateur
+app.post("/users", async (req, res) => {
+	const { first_name, last_name, birth_date, mail, password } = req.body;
+	const response = await createUser(first_name, last_name, birth_date, mail, password);
+	const status = response.split(" ")[0];
+
+	if (status === "error") {
+		return res.status(400).send(response);
+	}
+	res.status(201).send("inscription effectuée");
+})
+
+app.post("/books", async (req, res) => {
+	console.log(req.body);
+	const { cover, title, author, publish_year } = req.body;
+	const book = await addBook(cover, title, author, publish_year);
+	res.status(201).send(book);
+})
+
+app.get("/books", async (req, res) => {
+	const rows = await getBooks();
+	res.status(200).send(rows);
+})
+
+app.post("/login", async (req, res) => {
+
+	const { mail, password } = req.body;
+
+	if (!mail || !password) {
+		return res.status(400).json({ message: "Champs manquants" });
+	}
+
+	try {
+		const db = app.get("db");
+		const [rows] = await db.query('SELECT * FROM users WHERE mail = ?', [mail]);
+
+		if (rows.length === 0) {
+		return res.status(401).json({ error: "Utilisateur non trouvé." });
+		}
+
+		const user = rows[0];
+		const isMatch = await bcrypt.compare(password, user.password);
+
+		if (!isMatch) {
+		return res.status(401).json({ error: "Mot de passe incorrect." });
+		}
+		
+		const token = jwt.sign({ id: user.id, mail:user.mail }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+
+		res.status(200).json({ message: "Connexion réussie.",
+			user: {
+				id: user.id,
+				first_name: user.first_name,
+				last_name: user.last_name,
+				birth_date: user.birth_date,
+				mail: user.mail
+			},
+			token
+		 });
+
+	} catch (error) {
+	console.error(error);
+	res.status(500).json({ error: "Une erreur est survenue." });
+	}
+});
+
+
+
+
+app.get("/account" , checkToken, async (req, res) => {
+
+	res.send({"account": req.user});
+});
+
---
+++ api-library/config/db.js
@@ -0,0 +1,45 @@
+import mysql from 'mysql2/promise';
+import bcrypt from 'bcrypt';
+
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '', 
+  database: 'library'
+});
+
+export async function getBooks() {
+  const [rows] = await db.query('SELECT * FROM books')
+  return rows;
+}
+
+export async function getBooksById(id) {
+  const [rows] = await db.query('SELECT * FROM books WHERE id = ?; [id]')
+  return rows[0];
+}
+
+export async function getUser(id)
+{
+  const [rows] = await db.query('SELECT * FROM users WHERE id = ?', [id])
+  return rows[0]
+}
+
+export async function createUser(firstName ,lastName, mail, password)
+{
+  const [rows] = await db.query('SELECT * FROM users WHERE mail = ?', [mail]);
+
+  if( rows.length === 0){
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const [result] = await db.query('INSERT INTO users(first_name, last_name, mail, password) VALUES (?,?,?,?)',[firstName, lastName, mail, hashedPassword])
+    console.log(result)
+    return {
+      id: result.insertId,
+      first_name: firstName,
+      last_name: lastName,
+      mail: mail
+    }
+  } else {
+    return 'email déjà existant';
+  }
+}
